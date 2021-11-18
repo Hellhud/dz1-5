7b48fe4f43f74ed386202ac6bf0a35b5891a1d47
Hellhound <hardoneloves@gmail.com>
2021-11-18 15:35:48 +0300

    Добавьте файлы проекта.

---
+++ dz3/dz1and5.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+int main() {
+	extern int a, b, c, d;
+	a = 10;
+	b = 30;
+	c = 50;
+	d = 100;
+	extern float r,n;
+	r = static_cast <float> (c) / d;
+	std::cout << r << std::endl;
+	n = a * (b + r);
+	std::cout << n;
+}
